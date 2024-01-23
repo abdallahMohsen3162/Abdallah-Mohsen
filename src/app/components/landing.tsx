9fcf6bf1ae50df6bfdf4e62fe98b946fcf36193f
Abdallah Mohsen <93709847+abdallahMohsen3162@users.noreply.github.com>
2024-01-23 22:49:58 +0200

    Update landing.tsx

--- src/app/components/landing.tsx
+++ src/app/components/landing.tsx
@@ -23,7 +23,7 @@ export default function Landing() {
         <div className='desc'>
             <h1>Hi, I'm Abdallah Mohsen </h1>
             <h1>and I'm a  </h1>
-            <h1 className='job-title'>Fullstack Developer  </h1>
+            <h1 className='job-title'>Software engineer</h1>
         </div>
 
         <div className='visual'>
